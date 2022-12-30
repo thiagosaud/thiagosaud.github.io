f9e3309f9d0141ea7fcd05fa721f629881afcf12
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2022-12-29 22:35:31 -0300

    feat: 🎸 Cleaned Project

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/**
+ * @thiagosaud
+ * @description This file is unique in that it does a "lazy loading" of all files and main components which need to be loaded before other files!
+ */
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
